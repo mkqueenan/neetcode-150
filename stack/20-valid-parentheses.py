1cce1677ac2290ae64ab1c49533be8ba2fe1e62f
Kevin Queenan <kevin@queenan.dev>
2024-06-30 16:05:59 -0500

    Solution to "Validate Parentheses"
    https://neetcode.io/problems/validate-parentheses

--- stack/20-valid-parentheses.py
+++ stack/20-valid-parentheses.py
@@ -1,24 +1,25 @@
-def matching(open_: str, close_: str) -> bool:
-    pairs_ = {'(': ')', '{': '}', '[': ']'}
-    matching_ = pairs_.get(open_, None)
-    return matching_ == close_
+class Solution:
+    def matching(self, open_: str, close_: str) -> bool:
+        pairs_ = {'(': ')', '{': '}', '[': ']'}
+        matching_ = pairs_.get(open_, None)
+        return matching_ == close_
 
-
-def isValid( s: str) -> bool:
-    if not s:
-        return False
-    open_ = {'(', '{', '['}
-    valid_ = []
-    for char in s:
-        if char in open_:
-            valid_.append(char)
-        else:
-            try:
-                last_ = valid_.pop()
-                if matching(last_, char):
-                    continue
-                else:
+    def isValid(self, s: str) -> bool:
+        if not s:
+            return False
+        open_ = {'(', '{', '['}
+        close_ = {')', '}', ']'}
+        valid_ = []
+        for char in s:
+            if char in open_:
+                valid_.append(char)
+            else:
+                try:
+                    last_ = valid_.pop()
+                    if self.matching(last_, char):
+                        continue
+                    else:
+                        return False
+                except IndexError:
                     return False
-            except IndexError:
-                return False
-    return not valid_
+        return not valid_
